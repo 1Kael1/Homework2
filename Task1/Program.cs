12467594d0e867c1315f0cd7c262ec51ba73faa2
Kael <churushkin.kirill@mail.ru>
2022-10-23 19:47:50 +0300

    first commit

---
+++ Task1/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа. Выполнить с помощью числовых операций.
+
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+Console.WriteLine("Введите число");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int Digit(int num1) 
+{
+    int digit1 = num1 / 100; 
+    int digit2 = num1 / 10;
+    return  digit2 - digit1 * 10;
+}
+Console.WriteLine($"{Digit(num)}");  
